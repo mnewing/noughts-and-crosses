c9d1816d6c93894ca9d915dd3e264717514ed676
mnewing <mary_newing@hotmail.com>
2022-04-25 11:57:27 +0100

    Add files via upload

--- nAc.py
+++ nAc.py
@@ -1,14 +1,28 @@
-def getMove():
-    row = input("your move, which row")
-    col = input("your move, which col")
+def getMovePlayX():
+    row = input("your move, which row: ")
+    col = input("your move, which col: ")
+    if row == "1":
+        if col == "a" or col == "A" :
+            row1[0] = "X"
+        elif col == "B" or col == "b" :
+            row1[1] = "X"
+        else:
+            row1[1] = "X"
 
 def displayBoard():
     print("   A  | B | C ")
-    print(" 1    |   |   ")
+    print(" 1  "+row1[0]+" |   |   ")
     print("   -----------")
     print(" 2    |   |   ")
-    print("  -----------")
+    print("   -----------")
     print(" 3    |   |   ")
 
+#main program
+row1 = ["", "", ""]
+row2 = ["", "", ""]
+row3 = ["", "", ""]
+
+
 displayBoard()
-getMove()
+getMovePlayX()
+displayBoard()
